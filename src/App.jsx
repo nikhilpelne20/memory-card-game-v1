ab13c80da7edc76fdd58f852345222f62affbf04
nikhil pelne <nikhil788p@gmail.com>
2024-03-11 12:45:02 +0530

    plan out the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 function App() {
+  //Building a memory card game 
+  // components to build 
+  // --Header Section(logo)
+  // --Score Section (Best Score + current Score)
+  // --Card Section (Make a grid of 4*4)
+  // --Footer section(git logo and copyright)
 
   return (
     <>

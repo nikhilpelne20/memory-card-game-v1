69f59777ed7a9f3a78c5ea95fe9639a4b334f68f
nikhil pelne <nikhil788p@gmail.com>
2024-03-12 16:53:33 +0530

    add hover effect on cards

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ img, name }) {
   return (
-    <div className="w-[300px] h-[370px] bg-[#891652] flex flex-col items-center rounded-lg hover:shadow-yellow">
+    <div className="w-[300px] h-[370px] bg-[#891652] flex flex-col items-center rounded-lg hover:scale-105 duration-300 cursor-pointer">
       <div className="p-2">
         <img className="rounded-lg" src={img} alt={name} />
       </div>

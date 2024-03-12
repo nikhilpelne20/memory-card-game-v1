128eb2eb22add6460820c02cbd51940d5ebe049e
nikhil pelne <nikhil788p@gmail.com>
2024-03-12 12:14:41 +0530

    Implement data fetching with useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import Scores from './Scores/Scores'
 
 export default function Main() {
+  const [character,setCharacter]=useState(null);
+
+  useEffect(()=>{
+    const fetchCharacter = async ()=>{
+      const response = await fetch(`https://rickandmortyapi.com/api/character/1`);
+      const characterData = await response.json()
+      console.log(characterData)
+    }
+    fetchCharacter();
+  },[])
   return (
     <div>
         <Header/>

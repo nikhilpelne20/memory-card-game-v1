3d28c20a8317c53fd2598760bfc2df0fabfbcfd8
nikhil pelne <nikhil788p@gmail.com>
2024-03-13 15:20:03 +0530

    add git logo in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center pt-[5px] pb-[5px] bg-[#891652]">
+    <div className="flex justify-center pt-[5px] pb-[5px] bg-[#891652] items-center gap-2">
       <p className="text-2xl text-[#FFEDD8]">Copyright © 2024 nikhilpelne</p>
+      <a href="https://github.com/nikhilpelne20">
+    <FaGithub className="text-2xl text-[#FFEDD8]" />
+  </a>
     </div>
   );
 }
